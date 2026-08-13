287bb8ea7df012e58984dd9790679515f2091312
Juan David Zapata Barrera <163927311+JuanZB360@users.noreply.github.com>
2026-08-13 14:42:35 -0500

    Revert "creacion de componentes para la creacion de la pagina de perfiles"

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ConnectedUserProvider } from './contexts/ConnectedUser.context.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <ConnectedUserProvider>
-        <App />
-      </ConnectedUserProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
